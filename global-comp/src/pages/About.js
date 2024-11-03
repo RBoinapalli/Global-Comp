1e7362cd98d9ce556d532690f951b4a6fe905bd1
Rithika Boinapalli <rithika.boinapalli@gmail.com>
2024-11-03 18:21:58 -0500

    deploy #4

--- global-comp/src/pages/About.js
+++ global-comp/src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => {
         <div className="about-container">
           <div className="about-card">
             <div className="card-content">
-              <img src={missionImage} alt="Mission" className="card-image" />
+              
               <div className="card-text">
                 <h4>Our Mission</h4>
                 <p>We provide good career growth opportunities for information technology experts to fulfill their creative thoughts and ideas for business needs.</p>
@@ -22,7 +22,7 @@ const About = () => {
           </div>
           <div className="about-card">
             <div className="card-content">
-              <img src={visionImage} alt="Vision" className="card-image" />
+              
               <div className="card-text">
                 <h4>Our Vision</h4>
                 <p>We are a global innovative company that combines information technology expertise with technology to improve your business goals and fulfill end-user needs.</p>
@@ -31,7 +31,7 @@ const About = () => {
           </div>
           <div className="about-card">
             <div className="card-content">
-              <img src={goalImage} alt="Goal" className="card-image" />
+             
               <div className="card-text">
                 <h4>Our Goal</h4>
                 <p>We aim to improve user problems, business solutions, products' pain points, and motivations, adopting new artificial technology trends to transform businesses.</p>
